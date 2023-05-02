06f2871fea830b3964f74f99ef0ba0ffb7a736bc
Rof1yev <rofiyevdilshod@gmail.com>
2023-05-02 12:26:24 +0500

    image update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './home.css';
-import authorImage from '../../assets/author.jpg';
+import authorImage from '../../assets/Author.jpg';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
